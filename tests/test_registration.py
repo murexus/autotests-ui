b006affbd504ef83406a89fd06663ecbdfe8773f
murexus <murexus@gmail.com>
2026-01-06 19:05:37 +0300

    7.1 Знакомство с PageObject. Практика работы с Page Object

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -6,15 +6,13 @@ from pages.dashboard_page import DashboardPage
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration(chromium_page: Page):
+def test_successful_registration(registration_page: Page, dashboard_page: Page):
         email = "user@gmail.com"
         username = "user"
         password = "password"
 
-        registration_page = RegistrationPage(page=chromium_page)
         registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
         registration_page.fill_registration_form(email=email, username=username, password=password)
         registration_page.click_registration_button()
 
-        dashboard_page = DashboardPage(page=chromium_page)
         dashboard_page.check_visible_dashboard_title()
